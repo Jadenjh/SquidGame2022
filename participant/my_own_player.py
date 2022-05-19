10426a1dbdc841eafa9ed8b60be26d15ca65be79
Jadenjh <woghks0108@snu.ac.kr>
2022-05-19 17:03:56 +0900

    JH - 1st

--- participant/my_own_player.py
+++ participant/my_own_player.py
@@ -16,6 +16,7 @@ class my_own_player(part.Participant):
         # you can override this method in this sub-class
         # this method must contain 'self.initialize_params()' which is for initializing some essential variables
         # you can initialize what you define
+        print("A")
         self.initialize_params()
     # ====================================================================== for initializing your player every round
 
